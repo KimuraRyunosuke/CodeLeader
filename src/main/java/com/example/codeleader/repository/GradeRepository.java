9671565270ebfc2a78152df634b47941c93ab1fa
吉田大介 <daisuke@yoshidadaisukenoMacBook-Pro.local>
2023-09-22 15:10:16 +0900

    RankRepository.javaをGradeRepository.javaへ変更

--- src/main/java/com/example/codeleader/repository/GradeRepository.java
+++ src/main/java/com/example/codeleader/repository/GradeRepository.java
@@ -3,6 +3,6 @@ package com.example.codeleader.repository;
 import org.springframework.data.repository.CrudRepository;
 import com.example.codeleader.entity.User;
 
-public interface RankRepository extends CrudRepository<User, Long> {
+public interface GradeRepository extends CrudRepository<User, Long> {
     
 }
