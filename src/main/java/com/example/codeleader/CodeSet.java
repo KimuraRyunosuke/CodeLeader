d7254a26231095af6f8523004d86fc6a71f9b27f
吉田大介 <daisuke@yoshidadaisukenoMacBook-Pro.local>
2023-10-11 22:55:03 +0900

    コード出力用のデータ型として作成

---
+++ src/main/java/com/example/codeleader/CodeSet.java
@@ -0,0 +1,19 @@
+package com.example.codeleader;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+
+import com.example.codeleader.entity.Code;
+
+import lombok.Data;
+
+@Data
+public class CodeSet {
+    private String title;
+    private String comment;
+    private String lang;
+    private Timestamp postedAt;
+    private List<Code> codeList;
+
+}
