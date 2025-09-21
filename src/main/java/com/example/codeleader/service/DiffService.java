76025febb8345398164ea313f76b1edc0a1db677
KimuraRyunosuke <ciry22037@g.nihon-u.ac.jp>
2025-09-21 21:50:49 +0900

    modosi3

--- src/main/java/com/example/codeleader/service/DiffService.java
+++ src/main/java/com/example/codeleader/service/DiffService.java
@@ -20,8 +20,7 @@ public class DiffService {
 
         for (AbstractDelta<String> delta : patch.getDeltas()) {
             Map<String, Object> diffEntry = new HashMap<>();
-            // 差分タイプを日本語に変換して格納
-            diffEntry.put("type", localizeDiffType(delta.getType().toString()));
+            diffEntry.put("type", delta.getType().toString());
             diffEntry.put("source", delta.getSource().getLines());
             diffEntry.put("target", delta.getTarget().getLines());
             diffEntry.put("srcPos", delta.getSource().getPosition());
@@ -31,19 +30,5 @@ public class DiffService {
         }
 
         return diffs;
-    }
-
-    // 差分タイプを日本語に変換するメソッド
-    private String localizeDiffType(String type) {
-        switch (type.toUpperCase()) {
-            case "CHANGE":
-                return "変更";
-            case "INSERT":
-                return "追加";
-            case "DELETE":
-                return "削除";
-            default:
-                return "その他";
-        }
     }
 }
